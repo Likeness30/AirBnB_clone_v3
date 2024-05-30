6c8c83cbc393e95217b2e9d7a4e5ae4b74e90acf
Rufus <likeness14@gmail.com>
2024-05-30 19:52:39 +0100

    Update index.py file

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,14 +6,14 @@ module that runs the Flask app
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask_cors import CORS
-'''
+'''from os import getenv
+from flask_cors import CORS'''
+
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+'''cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})'''
 app.register_blueprint(app_views)
 
-
+'''
 @app.errorhandler(404)
 def page_not_found(exceptions):
     """error handler function"""
